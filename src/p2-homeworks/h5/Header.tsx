a05aa0825b576f406ff4961207d5f2d7417bb213
IvanRavinskiy <ivan.ravinskiy@gmail.com>
2022-01-30 13:01:13 +0300

    5 hw in progress v2

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
+import {PATH} from "./RoutesComponent";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
         </div>
     )
 }
